ac2d056e860d8b665fee1c4c7c32ea3e9a8eb233
fuxsociety <mushraf@quranium.org>
2025-02-22 12:56:44 +0530

    new position

--- components/pool/priceRange.tsx
+++ components/pool/priceRange.tsx
@@ -7,6 +7,8 @@ import { useCoinStore } from "@/store";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Button } from "@/components/ui/button";
 import { useStepContext } from "@/context/StepContext";
+import { RangeTabs } from "./RangeTabs";
+import { EditIcon } from "lucide-react";
 
 function generatePrice() {
   return (1000 + Math.random() * 100).toFixed(2); // Simulated price range between 1000-1100
@@ -16,7 +18,8 @@ export default function PriceRange() {
   const { coin1, coin2 } = useCoinStore();
   const { setCurrentStep } = useStepContext();
   const [data, setData] = useState([]);
-
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(0);
   useEffect(() => {
     const interval = setInterval(() => {
       setData((prevData) => {
@@ -31,6 +34,14 @@ export default function PriceRange() {
     return () => clearInterval(interval);
   }, []);
 
+  const handleSubmit = () => {
+    const data = {
+      minPrice,
+      maxPrice,
+    };
+    console.log(data);
+  };
+
   return (
     <main className="flex flex-col w-full space-y-10">
       <section className="p-5 rounded-[13px] w-full border flex flex-row space-x-5 justify-between">
@@ -39,6 +50,10 @@ export default function PriceRange() {
           <span>/</span>
           <span>{coin2.name}</span>
         </div>
+        <EditIcon
+          className="cursor-pointer"
+          onClick={() => setCurrentStep(1)}
+        />
       </section>
 
       <section className="w-full flex flex-col space-y-10 p-5 border rounded-[13px]">
@@ -56,27 +71,14 @@ export default function PriceRange() {
           </div>
         </div>
         <div>
-          <Tabs defaultValue="Full Range">
-            <TabsList className="grid w-full grid-cols-2">
-              <TabsTrigger className="p-2" value="Full Range">
-                Full Range
-              </TabsTrigger>
-
-              <TabsTrigger className="p-2" value="Custom Range">
-                Custom Range
-              </TabsTrigger>
-              <TabsContent value="Custom Range">Custom</TabsContent>
-              <TabsContent value="Full Range">
-                {/* <span>
-                  Providing full range liquidity ensures continuous market
-                  participation across all possible prices, offering simplicity
-                  but with potential for higher impermanent loss.
-                </span> */}
-              </TabsContent>
-            </TabsList>
-          </Tabs>
+          <RangeTabs
+            minPrice={minPrice}
+            maxPrice={maxPrice}
+            setMinPrice={setMinPrice}
+            setMaxPrice={setMaxPrice}
+          />
         </div>
-        <Button onClick={() => setCurrentStep(3)} className="red-btn">
+        <Button onClick={handleSubmit} className="red-btn">
           Continue
         </Button>
       </section>
