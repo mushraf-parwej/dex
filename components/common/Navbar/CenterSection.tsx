c749209f168ff40600b43cd93cd13a74e9c03e1e
fuxsociety <mushraf@quranium.org>
2025-02-27 02:57:32 +0530

    navabr updated

--- components/common/Navbar/CenterSection.tsx
+++ components/common/Navbar/CenterSection.tsx
@@ -1,16 +1,7 @@
 import React from "react";
 import { SearchInput } from "../SearchBar";
 const CenterSection = () => {
-  return (
-    <div className="hidden sm:flex justify-center flex-1 mx-4">
-      <SearchInput
-        placeholder="Search tokens..."
-        className="w-full max-w-[423px]"
-        value=""
-        onChange={() => {}}
-      />
-    </div>
-  );
+  return <div className="hidden sm:flex justify-center flex-1 mx-4"></div>;
 };
 
 export default CenterSection;
